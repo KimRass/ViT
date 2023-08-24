68df8cf61359e3447eceae2b183ca1c5c0866027
KiMRass <rmx1000@yonsei.ac.kr>
2023-08-24 23:16:30 +0900

    Training with ViT-Base

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ else:
 MULTI_GPU = True
 AUTOCAST = True
 # BATCH_SIZE = 4096 # "All models are trained with a batch size of 4096."
-BATCH_SIZE = 1024
+BATCH_SIZE = 512
 N_PRINT_EPOCHS = 4
 N_VAL_EPOCHS = 4
 N_EPOCHS = 200
