e26288e3b063c6e25566b2b737e8649d48b30a8f
KiMRass <rmx1000@yonsei.ac.kr>
2023-08-26 00:20:04 +0900

    RandomCrop

--- config.py
+++ config.py
@@ -65,7 +65,7 @@ else:
 MULTI_GPU = True
 AUTOCAST = True
 # BATCH_SIZE = 4096 # "All models are trained with a batch size of 4096."
-BATCH_SIZE = 512
+BATCH_SIZE = 2048
 N_PRINT_EPOCHS = 4
 N_VAL_EPOCHS = 4
 N_EPOCHS = 200
